4e09cf2d9e610202b42479f7ac00ea091988afb8
Jens Grochtdreis <jens@grochtdreis.de>
2025-04-27 15:06:26 +0200

    WebC-Plugin wieder entfernt

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,5 @@
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = (eleventyConfig) => {
-  //eleventyConfig.addPlugin(pluginWebc);
-
-  eleventyConfig.addPlugin(pluginWebc, {
-    components: [
-      // Add as a global WebC component
-      "npm:@oddbird/browser-support/*.webc",
-    ],
-  });
 
   eleventyConfig.addPassthroughCopy({
     'src/assets/css/main.css': '/assets/css/main.css',
